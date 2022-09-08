dbe17f15faa8bdd1eff28212cf8f709b1df013fb
masiv <masiviady7@gmail.com>
2022-09-09 00:10:19 +0200

    first commit

---
+++ src/Views/Movies/index.js
@@ -0,0 +1,38 @@
+import Modal from "../../components/Modal";
+import { Header } from "../../components/Header";
+import { useSelector, useDispatch } from "react-redux";
+import { LikeButton } from "../../components/LikeButton";
+
+const MoviesList = () => {
+  // const { images, loading, error, fetchImages } = useImageApi();
+
+  const movies = useSelector((state) => state.movie.movies);
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1 className="movie__title">Movies List</h1>
+
+        <div className="movie__list">
+          {movies.map((movie) => (
+            <div className="movie__item" key={movie.id}>
+              <h1> {movie.title} </h1>
+              <span> {movie.likes} j'aime </span>
+              <LikeButton movie={movie} />
+              <Modal>
+                <div className="">
+                  <h1>{movie.title}</h1>
+                  <img src="https://via.placeholder.com/150" alt="" />
+                  <p>{movie.overview}</p>
+                </div>
+              </Modal>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MoviesList;
---
+++ src/components/Modal.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import ModalContent from "./ModalContent";
+
+function Modal(props) {
+  const [open, setIsOpen] = useState(false);
+  return (
+    <div>
+      <button onClick={() => setIsOpen(true)} className="description">
+        Lire
+      </button>
+      {open && <ModalContent isOpen={setIsOpen}>{props.children}</ModalContent>}
+    </div>
+  );
+}
+
+export default Modal;
